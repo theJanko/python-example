8ca2e30e7d7e6e506e5ffc2aab7551bd0f697ca9
theJanko <michalwiecek94@gmail.com>
2020-10-11 17:19:00 +0200

    commit do zad. 5

--- hello.py
+++ hello.py
@@ -1,14 +1,30 @@
 from __future__ import print_function
 import sys
 
+def anything():
+    return "print"
 
 def hello(what):
     print('Hello, {}!'.format(what))
 
-
 def say_what():
     return 'world'
 
+def one():
+    a, b = 1, 2
+    return a + b
+
+def two():
+    a, b = "co", "de"
+    return a + b
+
+def three():
+    a, b = 1, 9
+    return b - a
+
+def four():
+    a, b = "zadanie", " 5."
+    return a + b
 
 def main():
     hello(say_what())
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,9 +1,26 @@
 import hello
 
 
+def test_anything():
+    print("zad 5")
+    assert hello.anything() == 'print'
+
 def test_says_world():
     assert hello.say_what() == 'world'
 
-
+"""
 def test_fail():
     assert hello.say_what == 'hey'
+"""
+
+def test_check_one():
+    assert hello.one() == 3
+
+def test_check_two():
+    assert hello.two() == "code"
+
+def test_check_three():
+    assert hello.three() == 8
+
+def test_check_four():
+    assert hello.four() == "zadanie 5."
